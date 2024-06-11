75fa8177423e265bd92e788746e18b87ec42d4a9
Liudmila Molkova <limolkova@microsoft.com>
2024-06-11 15:06:13 -0700

    remove 1.26

--- semconv/src/main/java/io/opentelemetry/semconv/SchemaUrls.java
+++ semconv/src/main/java/io/opentelemetry/semconv/SchemaUrls.java
@@ -6,7 +6,7 @@
 package io.opentelemetry.semconv;
 
 public final class SchemaUrls {
-  public static final String V1_26_0 = "https://opentelemetry.io/schemas/1.26.0";
+
   public static final String V1_25_0 = "https://opentelemetry.io/schemas/1.25.0";
   public static final String V1_24_0 = "https://opentelemetry.io/schemas/1.24.0";
   public static final String V1_23_1 = "https://opentelemetry.io/schemas/1.23.1";
