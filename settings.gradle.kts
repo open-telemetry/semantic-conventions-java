2fab8530dfd2589a8f81ed3b98c9494fde03844d
Trask Stalnaker <trask.stalnaker@gmail.com>
2024-12-02 10:24:08 -0800

    Update develocity configuration (#97)
    Co-authored-by: OpenTelemetry Bot <107717825+opentelemetrybot@users.noreply.github.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,13 +1,13 @@
 pluginManagement {
   plugins {
-    id("com.gradle.enterprise") version "3.14.1"
     id("de.undercouch.download") version "5.4.0"
     id("io.github.gradle-nexus.publish-plugin") version "1.3.0"
+    id("com.gradle.develocity") version "3.18.2"
   }
 }
 
 plugins {
-  id("com.gradle.enterprise")
+  id("com.gradle.develocity")
 }
 
 dependencyResolutionManagement {
@@ -17,47 +17,15 @@ dependencyResolutionManagement {
   }
 }
 
+develocity {
+  buildScan {
+    publishing.onlyIf { System.getenv("CI") != null }
+    termsOfUseUrl.set("https://gradle.com/help/legal-terms-of-use")
+    termsOfUseAgree.set("yes")
+  }
+}
+
 rootProject.name = "semantic-conventions-java"
 include(":dependencyManagement")
 include(":semconv-incubating")
 include(":semconv")
-
-val gradleEnterpriseServer = "https://ge.opentelemetry.io"
-val isCI = System.getenv("CI") != null
-val geAccessKey = System.getenv("GRADLE_ENTERPRISE_ACCESS_KEY") ?: ""
-
-// if GE access key is not given and we are in CI, then we publish to scans.gradle.com
-val useScansGradleCom = isCI && geAccessKey.isEmpty()
-
-if (useScansGradleCom) {
-  gradleEnterprise {
-    buildScan {
-      termsOfServiceUrl = "https://gradle.com/terms-of-service"
-      termsOfServiceAgree = "yes"
-      isUploadInBackground = !isCI
-      publishAlways()
-
-      capture {
-        isTaskInputFiles = true
-      }
-    }
-  }
-} else {
-  gradleEnterprise {
-    server = gradleEnterpriseServer
-    buildScan {
-      isUploadInBackground = !isCI
-
-      this as com.gradle.enterprise.gradleplugin.internal.extension.BuildScanExtensionWithHiddenFeatures
-      publishIfAuthenticated()
-      publishAlways()
-
-      capture {
-        isTaskInputFiles = true
-      }
-
-      gradle.startParameter.projectProperties["testJavaVersion"]?.let { tag(it) }
-      gradle.startParameter.projectProperties["testJavaVM"]?.let { tag(it) }
-    }
-  }
-}
