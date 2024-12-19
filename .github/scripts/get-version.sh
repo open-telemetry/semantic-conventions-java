5f4acc799703652e89fbe5cbd18110a338cada4f
Trask Stalnaker <trask.stalnaker@gmail.com>
2024-12-18 16:10:18 -0800

    Update change log for upcoming release (#123)

--- .github/scripts/get-version.sh
+++ .github/scripts/get-version.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -e
 
-grep -Eo "var semanticConventionsVersion = \".*\"" build.gradle.kts | grep -Eo "[0-9]+.[0-9]+.[0-9]+"
+grep "var semanticConventionsVersion = " build.gradle.kts | grep -Eo "[0-9]+.[0-9]+.[0-9]+"
