72523f010e7694f1f31006c0fe72fa3b6f106c19
Jack Berg <jberg@newrelic.com>
2025-01-06 14:53:27 -0600

    Remove -alpha suffix from opentelemetry-semconv artifact

--- semconv/build.gradle.kts
+++ semconv/build.gradle.kts
@@ -1,8 +1,6 @@
 plugins {
   id("otel.java-conventions")
   id("otel.publish-conventions")
-  // TODO: re-enable japicmp when artifact is stable
-  // id("otel.japicmp-conventions")
 
   id("otel.animalsniffer-conventions")
 }
