222df64f393f915b070a65c9edea04230a2f5b17
Trask Stalnaker <trask.stalnaker@gmail.com>
2025-01-27 13:04:02 -0800

    Fail of javadoc warnings (#100)

--- buildSrc/src/main/kotlin/otel.java-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/otel.java-conventions.gradle.kts
@@ -79,6 +79,8 @@ tasks {
       // TODO (trask) need to disable html validation until next semconv release
       //  see https://github.com/open-telemetry/semantic-conventions/pull/1664
       addBooleanOption("Xdoclint:all,-missing,-html", true)
+      // non-standard option to fail on warnings, see https://bugs.openjdk.java.net/browse/JDK-8200363
+      addStringOption("Xwerror", "-quiet")
     }
   }
 
