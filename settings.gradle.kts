d806213b7b42aa91dc5adcbaceb13a827ed1bb9e
Trask Stalnaker <trask.stalnaker@gmail.com>
2025-10-22 09:24:46 -0700

    Remove unnecessary condition for this repo

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -45,12 +45,6 @@ develocity {
     capture {
       fileFingerprints = true
     }
-
-    buildScanPublished {
-      java.io.File("build-scan.txt").printWriter().use { writer ->
-        writer.println(buildScanUri)
-      }
-    }
   }
 }
 
