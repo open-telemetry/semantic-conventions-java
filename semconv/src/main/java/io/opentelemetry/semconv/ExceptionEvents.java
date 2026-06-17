4d55cf897ebdc7486780ecf35be32e989ae3b2d3
Jack Berg <34418638+jack-berg@users.noreply.github.com>
2026-06-17 15:38:58 -0500

    Use javadoc formatter, publish stable event constants, link deprecated incubating event constants to stable equivalent

---
+++ semconv/src/main/java/io/opentelemetry/semconv/ExceptionEvents.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.semconv;
+
+// DO NOT EDIT, this is an Auto-generated file from
+// buildscripts/templates/registry/java/SemanticEvents.java.j2
+@SuppressWarnings("unused")
+public final class ExceptionEvents {
+  /** This event describes a single exception. */
+  public static final String EXCEPTION = "exception";
+
+  private ExceptionEvents() {}
+}
