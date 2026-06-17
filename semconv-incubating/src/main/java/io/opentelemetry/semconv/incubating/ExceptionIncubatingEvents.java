c8b90b339c535a350112860ce636a96ebc7c8eca
Jack Berg <34418638+jack-berg@users.noreply.github.com>
2026-06-17 15:43:21 -0500

    Disable stable event constant generation

--- semconv-incubating/src/main/java/io/opentelemetry/semconv/incubating/ExceptionIncubatingEvents.java
+++ semconv-incubating/src/main/java/io/opentelemetry/semconv/incubating/ExceptionIncubatingEvents.java
@@ -9,13 +9,8 @@ package io.opentelemetry.semconv.incubating;
 // buildscripts/templates/registry/incubating_java/IncubatingSemanticEvents.java.j2
 @SuppressWarnings("unused")
 public final class ExceptionIncubatingEvents {
-  /**
-   * This event describes a single exception.
-   *
-   * @deprecated deprecated in favor of stable {@link
-   *     io.opentelemetry.semconv.ExceptionEvents#EXCEPTION} event.
-   */
-  @Deprecated public static final String EXCEPTION = "exception";
+  /** This event describes a single exception. */
+  public static final String EXCEPTION = "exception";
 
   private ExceptionIncubatingEvents() {}
 }
